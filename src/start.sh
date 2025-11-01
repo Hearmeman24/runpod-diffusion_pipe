9144f48389c98b13245d10d6b178a575978c7c39
avivkaplan <avivkaplan00@gmail.com>
2025-11-01 13:33:31 +0200

    fixed toml file

--- src/start.sh
+++ src/start.sh
@@ -123,6 +123,9 @@ fi
 echo "Installing torch 2.7.1"
 pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu128
 
+echo "Upgrading transformers package..."
+pip install transformers -U
+
 echo "Installing huggingface-cli..."
 pip install --upgrade "huggingface_hub[cli]"
 
